6a167f0ef7e151eec66ac455cecc5ce5fa12522e
ftmzhrsckldr <36725373+ftmzhrsckldr@users.noreply.github.com>
2025-08-15 14:03:39 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,11 @@
+def env_has_key(key)
+  value = ENV[key]
+  value.nil? || value.strip.empty? ? abort_with_message("Missing #{key}") : value
+end
+
+test_env = env_has_key("AC_TEST_ENV")
+value = Date.today.to_s
+
+File.open(env_has_key("AC_ENV_FILE_PATH"), 'a') do |f|
+  f.puts "#{AC_TEST_ENV}=#{value}"
+end
